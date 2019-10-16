2c9481b20fb94e410f4822f6e0cce78370f7118d
reece <consolelogreece@gmail.com>
2019-10-16 16:34:41 +0100

    inital boilerplate for cache testing

---
+++ GameHub.Test/Caching/Cache.cs
@@ -0,0 +1,18 @@
+using Xunit;
+// sets properly
+// gets properly
+/// accessing updates expire time
+// item deletes after
+
+namespace GameHub.Test.Caching
+{
+    
+    public class Getting
+    {
+        [Fact]
+        public void GetsProperlty()
+        {
+
+        }
+    }
+}
