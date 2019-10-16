48105d941a92c88a188e442d5e896c82c99a709f
reece <consolelogreece@gmail.com>
2019-10-16 04:05:46 +0100

    added some tests for moving in connect four

--- GameHub.Test/BoardGames/ConnectFour.cs
+++ GameHub.Test/BoardGames/ConnectFour.cs
@@ -152,5 +152,75 @@ namespace GameHub.Test.BoardGames
                 Assert.True(canStartGameWithTwoPlayers, "Was unable to start game with 2 players registered");
             }
         }
+
+        public class Moves
+        {
+            private static readonly string player1Id = "1234";
+            private static readonly string player2Id = "abcd";
+            private ConnectFour GetInitiatedGame()
+            {
+                var game = GetGame(GetDefaultConfig(player1Id));
+
+                game.RegisterPlayer(player1Id, "user");
+                game.RegisterPlayer(player2Id, "user");
+
+                return game;
+            }
+
+            [Fact]
+            public void CanMove()
+            {
+                // arrange
+                var game = GetInitiatedGame();
+                game.StartGame(player1Id);
+
+                // act
+                var movedSuccessfully = game.MakeMove(1, player1Id);
+
+                Assert.True(movedSuccessfully, "Failed to move despite all conditions being fine");
+            }
+
+            [Fact]
+            public void CantMoveIfNotTurn()
+            {
+                // arrange
+                var game = GetInitiatedGame();
+                game.StartGame(player1Id);
+
+                // act
+                var movedSuccessfully = game.MakeMove(1, player2Id);
+
+                Assert.False(movedSuccessfully, "Player was able to move despite not being their turn");
+            }
+
+            // this checks to make sure the game keeps track of who's turn it is.
+            [Fact]
+            public void CantMoveTwiceInARow()
+            {
+                // arrange
+                var game = GetInitiatedGame();
+                game.StartGame(player1Id);
+
+                // act
+                var move1Successful = game.MakeMove(1, player1Id);
+                var move2Successful = game.MakeMove(1, player1Id);
+
+                Assert.True(move1Successful, "Player was unable to move despite it being their turn");
+                Assert.False(move2Successful, "Player moved despite it not being their turn");
+            }
+
+            public void CantMoveOnInvalidColumn()
+            {
+                 // arrange
+                var game = GetInitiatedGame();
+                game.StartGame(player1Id);
+
+                // act
+                // Important to node that game is using the default configuration, which is 7 columns.
+                var moveSuccessful = game.MakeMove(100, player1Id);
+
+                Assert.False(moveSuccessful, "Was able to move on a column that does not exist");
+            }
+        }
     }
 }
