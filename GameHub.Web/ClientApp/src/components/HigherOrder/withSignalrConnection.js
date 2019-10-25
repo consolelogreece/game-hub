58884fff8d1e00b00aca0ce93faea34346c2565f
reece <consolelogreece@gmail.com>
2019-10-25 02:19:26 +0100

    updated signalr connection hoc on method to accept arrays

--- GameHub.Web/ClientApp/src/components/HigherOrder/withSignalrConnection.js
+++ GameHub.Web/ClientApp/src/components/HigherOrder/withSignalrConnection.js
@@ -20,7 +20,21 @@ export default function(WrappedComponent, endpoint)
 
         invoke = (destination, ...params) => this.state.connection.invoke(destination, ...this.state.permanentInvokeParams, ...params);
 
-        on = (destination, func) => this.state.connection.on(destination, func);
+        on = (destination, func) => 
+        {
+            switch (typeof(destination))
+            {
+                case "array":
+                    destination.forEach(el => {
+                        if (typeof(el) == "string")
+                            this.state.connection.on(el, func)
+                    });
+
+                case "string":
+                    this.state.connection.on(el, func);
+                    break;
+            }
+        };
 
         startConnection = () => this.state.connection.start();
 
