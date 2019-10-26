32b8da90a14292cc998671e3a91708d071259286
reece <consolelogreece@gmail.com>
2019-10-26 01:56:23 +0100

    updated minimum values for c4 config on front end

--- GameHub.Web/ClientApp/src/components/ConnectFour/NewGameC4.js
+++ GameHub.Web/ClientApp/src/components/ConnectFour/NewGameC4.js
@@ -40,19 +40,19 @@ export class NewGameC4 extends Component {
             <div>
                 <h6>Rows</h6>
                 {!!this.state.errors.nRows && <ErrorMessage text={this.state.errors.nRows} />}
-                <input name="nRows" value={this.state.roomConfig.nRows} min="0" max="30" type="number" onChange={e => this.HandleChange(e)} />
+                <input name="nRows" value={this.state.roomConfig.nRows} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
                 <br />
                 <h6>Columns</h6>
                 {!!this.state.errors.nCols && <ErrorMessage text={this.state.errors.nCols} />}
-                <input name="nCols" value={this.state.roomConfig.nCols} min="0" max="30" type="number" onChange={e => this.HandleChange(e)} />
+                <input name="nCols" value={this.state.roomConfig.nCols} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
                 <br />
                 <h6>Win Threshold</h6>
                 {!!this.state.errors.winThreshold && <ErrorMessage text={this.state.errors.winThreshold} />}
-                <input name="winThreshold" value={this.state.roomConfig.winThreshold} min="0" max="30" type="number" onChange={e => this.HandleChange(e)} />
+                <input name="winThreshold" value={this.state.roomConfig.winThreshold} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
                 <br />
                 <h6>Max Players</h6>
                 {!!this.state.errors.nPlayersMax && <ErrorMessage text={this.state.errors.nPlayersMax} />}
-                <input name="nPlayersMax" value={this.state.roomConfig.nPlayersMax} min="0" max="8" type="number" onChange={e => this.HandleChange(e)} />
+                <input name="nPlayersMax" value={this.state.roomConfig.nPlayersMax} min="2" max="8" type="number" onChange={e => this.HandleChange(e)} />
                 <br />
                 {/* Only render general error message if there are no other errors. */}
                 {!!this.state.errors.general && Object.keys(this.state.errors).length === 1 && <ErrorMessage text={this.state.errors.general} />}
