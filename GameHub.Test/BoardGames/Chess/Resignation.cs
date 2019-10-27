21aba7aed432125a59ee6624c3e6e9b644f1f90d
reece <consolelogreece@gmail.com>
2019-10-27 04:28:40 +0000

    added more chess tests

---
+++ GameHub.Test/BoardGames/Chess/Resignation.cs
@@ -0,0 +1,53 @@
+using Xunit;
+
+namespace GameHub.Test.BoardGames.ChessTests
+{
+    public class Resignation
+    {
+        [Fact]
+        public void CanResign()
+        {
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            game.RegisterPlayer("1234", "player1");
+            game.RegisterPlayer("abcd", "player2");
+
+            game.StartGame("1234");
+
+            var resignResult = game.Resign("1234");
+
+            Assert.True(resignResult.WasSuccessful);
+        }
+
+        [Fact]
+        public void CantResignIfGameNotStarted()
+        {
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            game.RegisterPlayer("1234", "player1");
+            game.RegisterPlayer("abcd", "player2");
+
+            var resignResult = game.Resign("1234");
+
+            Assert.False(resignResult.WasSuccessful);
+        }
+
+
+        [Fact]
+        public void CantResignTwice()
+        {
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            game.RegisterPlayer("1234", "player1");
+            game.RegisterPlayer("abcd", "player2");
+
+            game.StartGame("1234");
+
+            var resignResult1 = game.Resign("1234");
+            var resignResult2 = game.Resign("1234");
+
+            Assert.True(resignResult1.WasSuccessful);
+            Assert.False(resignResult2.WasSuccessful);
+        }
+    }
+}
