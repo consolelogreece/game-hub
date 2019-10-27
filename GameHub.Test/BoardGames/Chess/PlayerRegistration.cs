87680549348a773232ac86afa307ddb3cf7030d5
reece <consolelogreece@gmail.com>
2019-10-27 04:25:35 +0000

    added chess tests

--- GameHub.Test/BoardGames/Chess/PlayerRegistration.cs
+++ GameHub.Test/BoardGames/Chess/PlayerRegistration.cs
@@ -34,5 +34,100 @@ namespace GameHub.Test.BoardGames.ChessTests
             // assert
              Assert.False(registrationResult.WasSuccessful, "Was able to register player after game has already started");
         }  
+
+
+        public void CantRegisterMoreThanMaxPlayers()
+        {
+            // arrange
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            // act
+            game.RegisterPlayer("1234", "user");
+            game.RegisterPlayer("abcd", "user2");
+
+            var registrationResult = game.RegisterPlayer("zxcv", "user3");
+
+            // assert
+            Assert.False(registrationResult.WasSuccessful, "Was able to register player despite game being full");
+        }
+
+        [Fact]
+        public void CantRegisterSameIDTwice()
+        {
+            // arrange
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            // act
+            game.RegisterPlayer("1234", "user");
+            
+            var registrationResult = game.RegisterPlayer("1234", "user2");
+
+            // assert
+            Assert.False(registrationResult.WasSuccessful, "Was able to register player ID already being in use");  
+        }
+
+        [Fact]
+        public void CantRegisterSameNickTwice()
+        {
+            // arrange
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            // act
+            game.RegisterPlayer("1234", "user");
+            
+            var registrationResult = game.RegisterPlayer("1234", "user");
+
+            // assert
+            Assert.False(registrationResult.WasSuccessful, "Was able to register player nick which is already in use");  
+        }
+
+        [Fact]
+        public void CanGetPlayer()
+        {
+            // arrange
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            // act
+            game.RegisterPlayer("1234", "user");
+
+            var player = game.GetPlayer("1234");
+
+            // assert
+            Assert.NotNull(player);
+        }
+
+        [Fact]
+        public void GetsCorrectPlayer()
+        {
+            // arrange
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+
+            // act
+            game.RegisterPlayer("1234", "user");
+            game.RegisterPlayer("abcd", "user2");
+
+            var player = game.GetPlayer("1234");
+
+            // assert
+            Assert.Equal(player.Id, "1234");
+        }
+
+        [Fact]
+        public void SetsHostCorrectly()
+        {
+            // arrange
+            var game = ChessTestHelpers.GetGame(ChessTestHelpers.GetDefaultConfig("1234"));
+            
+            // act
+            game.RegisterPlayer("1234", "user");
+            game.RegisterPlayer("abcd", "user2");
+
+            var playerHost = game.GetPlayer("1234");
+            var playerNormal = game.GetPlayer("abcd");
+
+            // assert
+            Assert.True(playerHost.IsHost, "Not setting host flag to true on host player");
+            Assert.False(playerNormal.IsHost, "Incorrectly setting host flag to true on normal player");
+        }
     }
 }
