d6579068c864a9a72fd1c12079617e34485e80fd
reece <consolelogreece@gmail.com>
2019-10-29 04:04:55 +0000

    connect four new game now interacts with controller through post request to create a game instead of creating a signalr connection only for game creation.

--- GameHub.Web/ClientApp/src/components/ConnectFour/NewGameC4.js
+++ GameHub.Web/ClientApp/src/components/ConnectFour/NewGameC4.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
 import  ErrorMessage from '../Common/ErrorMessage';
+import axios from 'axios';
 
 export class NewGameC4 extends Component {
     constructor(props) {
@@ -15,48 +16,42 @@ export class NewGameC4 extends Component {
         };
     }
 
-    componentDidMount() 
-    {  
-        this.props.startConnection()
-            .then(() => console.log('Connection started!'))
-            .catch(err => console.log('Error while establishing connection :(', err))
-
-        this.props.on("IllegalConfiguration", errors => this.setState({errors: errors}))
-    }
-
-    CreateRoom = () => 
-    {
-        this.props.invoke('CreateRoom', this.state.roomConfig)
-            .then(gameId => this.props.history.push(gameId))
-            .catch(err => console.error(err));
-    }
-
     HandleChange(e) {
         this.setState({ ...this.state, roomConfig: { ...this.state.roomConfig, [e.target.name]: e.target.value }})
+    }
+
+    CreateRoom = e =>
+    {
+        e.preventDefault();
+        axios.post('/api/connectfour/createroom', this.state.roomConfig)
+        .then(res => this.props.history.push(res.data))
+        .catch(res => this.setState({errors: res.response.data}))
     }
 
     render() {
         return (
             <div>
-                <h6>Rows</h6>
-                {!!this.state.errors.nRows && <ErrorMessage text={this.state.errors.nRows} />}
-                <input name="nRows" value={this.state.roomConfig.nRows} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
-                <br />
-                <h6>Columns</h6>
-                {!!this.state.errors.nCols && <ErrorMessage text={this.state.errors.nCols} />}
-                <input name="nCols" value={this.state.roomConfig.nCols} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
-                <br />
-                <h6>Win Threshold</h6>
-                {!!this.state.errors.winThreshold && <ErrorMessage text={this.state.errors.winThreshold} />}
-                <input name="winThreshold" value={this.state.roomConfig.winThreshold} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
-                <br />
-                <h6>Max Players</h6>
-                {!!this.state.errors.nPlayersMax && <ErrorMessage text={this.state.errors.nPlayersMax} />}
-                <input name="nPlayersMax" value={this.state.roomConfig.nPlayersMax} min="2" max="8" type="number" onChange={e => this.HandleChange(e)} />
-                <br />
-                {/* Only render general error message if there are no other errors. */}
-                {!!this.state.errors.general && Object.keys(this.state.errors).length === 1 && <ErrorMessage text={this.state.errors.general} />}
-                <button onClick={() => this.CreateRoom()}>Create</button>
+                <form onSubmit={this.CreateRoom}>
+                    <h6>Rows</h6>
+                    {!!this.state.errors.nRows && <ErrorMessage text={this.state.errors.nRows} />}
+                    <input name="nRows" value={this.state.roomConfig.nRows} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
+                    <br />
+                    <h6>Columns</h6>
+                    {!!this.state.errors.nCols && <ErrorMessage text={this.state.errors.nCols} />}
+                    <input name="nCols" value={this.state.roomConfig.nCols} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
+                    <br />
+                    <h6>Win Threshold</h6>
+                    {!!this.state.errors.winThreshold && <ErrorMessage text={this.state.errors.winThreshold} />}
+                    <input name="winThreshold" value={this.state.roomConfig.winThreshold} min="2" max="30" type="number" onChange={e => this.HandleChange(e)} />
+                    <br />
+                    <h6>Max Players</h6>
+                    {!!this.state.errors.nPlayersMax && <ErrorMessage text={this.state.errors.nPlayersMax} />}
+                    <input name="nPlayersMax" value={this.state.roomConfig.nPlayersMax} min="2" max="8" type="number" onChange={e => this.HandleChange(e)} />
+                    <br />
+                    {/* Only render general error message if there are no other errors. */}
+                    {!!this.state.errors.general && Object.keys(this.state.errors).length === 1 && <ErrorMessage text={this.state.errors.general} />}
+                    <button>Create</button>
+                </form>
             </div>
         )
     }
