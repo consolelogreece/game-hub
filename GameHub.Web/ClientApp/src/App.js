2256ceb458a5fe691fcbc85e0e94c70d9f7184c5
reece <consolelogreece@gmail.com>
2019-10-29 23:46:52 +0000

    newgamec4 no longer uses signalr hoc

--- GameHub.Web/ClientApp/src/App.js
+++ GameHub.Web/ClientApp/src/App.js
@@ -21,7 +21,7 @@ export default class App extends Component {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/games' component={gamesPage} />
                 <Route exact path='/about' component={aboutPage} />
-                <Route exact path='/connectfour/createroom' component={withSignalrConnection(NewGameC4, '/connectfourhub')} />
+                <Route exact path='/connectfour/createroom' component={NewGameC4} />
                 <Route exact path='/connectfour/:gameId' component={withSignalrConnection(ConnectFour, '/connectfourhub')} />
                 <Route exact path='/chess/createroom' component={withSignalrConnection(NewGameChess, '/chesshub')} />
                 <Route exact path='/chess/:gameId' component={withSignalrConnection(ResizeWithContainerHOC(Chess), '/chesshub')} />
