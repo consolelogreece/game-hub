878f22d86a8bce6827c7dfeb18b1a43e3ddcff40
reece <consolelogreece@gmail.com>
2019-11-02 15:04:00 +0000

    error messages no transition in/out

--- GameHub.Web/ClientApp/src/components/Common/Forms/FormRegion/index.js
+++ GameHub.Web/ClientApp/src/components/Common/Forms/FormRegion/index.js
@@ -1,18 +1,59 @@
-import React from 'react'
+import React from 'react';
 import ErrorMessage from '../../ErrorMessage';
 import './styles.css'
 import { faUnderline } from '@fortawesome/free-solid-svg-icons';
 
 // name of error related classes in styles.css
-const errorStyle = "form-region-error";
-const noErrorStyle = "form-region-no-error";
+const errorStyleInput = "form-region-error";
+const noErrorStyleInput = "form-region-no-error";
+const errorStyleMessage = "form-region-error-message-show";
+const noErrorStyleMessage = "form-region-error-message-hide";
 
 export default class FormRegion extends React.PureComponent {
 
+    constructor(props)
+    {
+        super(props)
+        {
+            this.state = {
+                errorMessage: "",
+                messageRenderClassName:errorStyleMessage,
+                inputRenderClassName:""
+            };
+        }
+    }
+
+    componentDidUpdate(prevProps, _, __)
+    {
+        if (prevProps.errors !== undefined && prevProps.errors.length !== 0)
+        {
+            if (this.props.errors === undefined || this.props.errors.length === 0)
+            {
+                this.setState({messageRenderClassName: noErrorStyleMessage}, () => {
+                    setTimeout(() => {
+                        this.setState({
+                            errorMessage: this.props.errors,
+                            messageRenderClassName: errorStyleMessage
+                        })
+                    }, 300)
+                });
+            }
+        }
+        else
+        {
+            if (this.state.messageRenderClassName !== noErrorStyleMessage)
+            {
+                this.setState({
+                    errorMessage: this.props.errors
+                });
+            }
+        }
+    }
+
     render()
     {
-        let hasErrors = !(this.props.errors === undefined || this.props.errors.length === 0);
-        let errorDependentClass = hasErrors ? errorStyle : noErrorStyle;
+        let hasErrors = !(this.state.errorMessage === undefined || this.state.errorMessage.length === 0);
+        let errorDependentClass = hasErrors ? errorStyleInput : noErrorStyleInput;
 
         return (
             <div className={`form-region ${errorDependentClass}`}>
@@ -21,8 +62,10 @@ export default class FormRegion extends React.PureComponent {
                 </div>
                 {this.props.children}
                 {hasErrors && (
-                    <div className="form-region-error-message">
-                        <ErrorMessage text={this.props.errors} />
+                    <div className={this.state.messageRenderClassName}>
+                        <div className="form-region-error-message">
+                            <ErrorMessage text={this.state.errorMessage} />
+                        </div>
                     </div>
                 )}
             </div>
