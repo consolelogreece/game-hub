2d91d11e3ce8ae725dd9a6d8ca6e081b06940af5
reece <consolelogreece@gmail.com>
2019-11-02 14:08:44 +0000

    adjusted color of error classes in css

--- GameHub.Web/ClientApp/src/components/ConnectFour/LandingPage.js
+++ GameHub.Web/ClientApp/src/components/ConnectFour/LandingPage.js
@@ -4,6 +4,7 @@ import NewGameForm from './NewGameForm';
 export default props => {
     return (
         <div>
+            <h6>Connect Four</h6>
             <NewGameForm history={props.history}/>
         </div>
     )
