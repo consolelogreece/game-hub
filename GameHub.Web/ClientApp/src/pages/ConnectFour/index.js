fbdc0597c164bbf825bf2af92464386adc9a1cf0
reece <consolelogreece@gmail.com>
2019-11-03 13:14:39 +0000

    made c4 landing page stateful and the create new game form is now a popup

--- GameHub.Web/ClientApp/src/pages/ConnectFour/index.js
+++ GameHub.Web/ClientApp/src/pages/ConnectFour/index.js
@@ -1,23 +1,47 @@
 import React from 'react'
 import NewGameForm from '../../components/ConnectFour/NewGameForm';
 import { Subtitle, Title} from '../../components/Common/Text';
+import Popup from '../../components/Common/Popup';
 
-export default props => {
-    return (
-        <div>
-            <Title text="Connect Four" />
-            <Subtitle style={{marginTop: "40px"}}>How to win</Subtitle>
-            <p>
-                To win Connect Four you must be the first player to get four of your colored tokens in a row either horizontally, vertically or diagonally. 
-            </p>
-            <NewGameForm history={props.history}/>
-            <Subtitle style={{marginTop: "40px"}}>Key</Subtitle>
-            <p>
-                Rows: The amount of rows in your board (max 30). <br />
-                Columns: The amount of columns in your board (max 30). <br />
-                Win Threshold: The amount of tokens in a row needed to win. <br />
-                Maximum Players: The maximum number of players able to join your game.
-            </p>
-        </div>
-    )
+export default class ConnectFourLandingPage extends React.PureComponent {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            displayPopup: false
+        }
+    }
+
+    togglePopup()
+    {
+        this.setState({displayPopup: !this.state.displayPopup});
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <Title text="Connect Four" />
+                <Subtitle style={{marginTop: "40px"}}>How to win</Subtitle>
+                <p>
+                    To win Connect Four you must be the first player to get four of your colored tokens in a row either horizontally, vertically or diagonally. 
+                </p>
+                { this.state.displayPopup && (
+                    <Popup onClose={this.togglePopup}>
+                        <NewGameForm history={this.props.history}/>
+                    </Popup> 
+                )}
+                <button onClick={() => this.togglePopup()}>
+                    Create Game
+                </button>
+                <Subtitle style={{marginTop: "40px"}}>Key</Subtitle>
+                <p>
+                    Rows: The amount of rows in your board (max 30). <br />
+                    Columns: The amount of columns in your board (max 30). <br />
+                    Win Threshold: The amount of tokens in a row needed to win. <br />
+                    Maximum Players: The maximum number of players able to join your game.
+                </p>
+            </div>
+        )
+    }
 }
