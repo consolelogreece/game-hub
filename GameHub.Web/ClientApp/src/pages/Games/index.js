41e13a1aa571f34f198fba092289494d8c1e8a03
reece <consolelogreece@gmail.com>
2019-11-05 14:55:48 +0000

    added animations on opening/closing new game form

--- GameHub.Web/ClientApp/src/pages/Games/index.js
+++ GameHub.Web/ClientApp/src/pages/Games/index.js
@@ -8,6 +8,12 @@ import LoadingScreen from '../../components/Common/LoadingScreen';
 import Popup from '../../components/Common/Popup';
 import ConnectFourForm from '../../forms/ConnectFour';
 import ChessForm from '../../forms/Chess';
+import './styles.css';
+
+const showFormClass = "game-form-show";
+const hideFormClass = "game-form-hide";
+const showPopupClass = "game-popup-show";
+const hidePopupClass = "game-popup-hide";
 
 export default class Games extends Component {
 
@@ -22,7 +28,8 @@ export default class Games extends Component {
         "Connect Four": ConnectFourForm,
         "Chess": ChessForm
       },
-      selectedForm:""
+      selectedForm:"",
+      renderPopup: false
     }
   }
   
@@ -53,9 +60,25 @@ export default class Games extends Component {
     }
   }
 
+  closePopup = () =>
+  {
+    this.setState({renderPopup: false}, () => {
+      setTimeout(() => {
+          this.setState({
+              selectedForm: ""
+          })
+      }, 300)
+    });
+  }
+
+  openPopup = selectedForm =>
+  {
+    this.setState({renderPopup: true, selectedForm: selectedForm})
+  }
+
   render () {
     let games = this.state.games.map(g => (
-      <div style={{cursor: "pointer"}} onClick={() => this.setState({selectedForm: g.name})}>
+      <div style={{cursor: "pointer"}} onClick={() => this.openPopup(g.name)}>
         <HoverFadeOverlay text={g.name} fadeColor={"rgba(0,0,0,0.8)"}>
           <Card {...g} onImageLoad={this.onImageLoad}/> 
         </HoverFadeOverlay>
@@ -70,15 +93,24 @@ export default class Games extends Component {
       return <Form history={this.props.history}/>
     })(this.state.selectedForm, this.state.formMap);
 
-      console.log(Form)
+    let renderDependentClassForm = this.state.renderPopup ? showFormClass : hideFormClass;
+    let renderDependentClassPopup =  this.state.renderPopup ? showPopupClass : hidePopupClass;
+
     return (
       <div>
         <LoadingScreen render={this.state.loading}/>
-        {Form !== "" && (
-          <Popup style={{width:" 100%", maxWidth: "700px"}} onClose={() => this.setState({selectedForm: ""})}>
-            {Form}
-          </Popup>
-        )} 
+          {Form !== "" && (
+            <div id="games-popup-container">
+              <Popup 
+                superContainerClassNames={renderDependentClassPopup} 
+                containerClassNames={renderDependentClassForm} 
+                popupStyles={{width:" 100%", maxWidth: "700px"}} 
+                superContainerStyles={{backgroundColor: "rgba(0,0,0, 0.5)"}}
+                onClose={() => this.closePopup()}>
+                {Form}
+              </Popup>
+            </div>
+          )}
         <div style={{margin: "30px 0px"}}>
           <Title text="Games"/> 
         </div>
