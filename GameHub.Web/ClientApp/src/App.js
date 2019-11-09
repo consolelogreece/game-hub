bc3fa308854c9f10c36264c0094ca4ca70e327db
reece <consolelogreece@gmail.com>
2019-11-09 03:39:10 +0000

    started adding a context for loading screen, to have loading screen in on place

--- GameHub.Web/ClientApp/src/App.js
+++ GameHub.Web/ClientApp/src/App.js
@@ -6,6 +6,7 @@ import aboutPage from './pages/About';
 import ConnectFourPage from './pages/ConnectFour'
 import ChessPage from './pages/Chess';
 import './styles.css';
+import { LoadingProvider } from  './context/Loading';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -13,12 +14,14 @@ export default class App extends Component {
   render () {
     return (
         <Layout>
-            <Switch>
-                <Route exact path='/about' component={aboutPage} />
-                <Route exact path='/connectfour/:gameId' component={ConnectFourPage} />
-                <Route exact path='/chess/:gameId' component={ChessPage} />
-                <Route path='/' component={gamesPage} />
-            </Switch>
+            <LoadingProvider>
+              <Switch>
+                  <Route exact path='/about' component={aboutPage} />
+                  <Route exact path='/connectfour/:gameId' component={ConnectFourPage} />
+                  <Route exact path='/chess/:gameId' component={ChessPage} />
+                  <Route path='/' component={gamesPage} />
+              </Switch>
+            </LoadingProvider>
       </Layout>
     );
   }
---
+++ GameHub.Web/ClientApp/src/context/Loading/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import LoadingScreen from '../../components/Common/LoadingScreen'
+
+export let LoadingCtx = React.createContext();
+
+export class LoadingProvider extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: false
+      };
+    }
+
+    update = loading => {
+       this.setState({loading: loading});
+    }
+
+    render() {
+        return (
+           <LoadingCtx.Provider value={{update: this.update}}>      
+              <LoadingScreen render={this.state.loading} /> 
+              {this.props.children}
+           </LoadingCtx.Provider>
+       )
+    }
+}
