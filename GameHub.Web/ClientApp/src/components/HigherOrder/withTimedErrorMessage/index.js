03ad170298e0ac81aef69c0c4b4fd4d9c8fcd7cf
reece <consolelogreece@gmail.com>
2020-01-14 03:22:40 +0000

    refactored popup messages now to be more DRY compliant

--- GameHub.Web/ClientApp/src/components/HigherOrder/withTimedErrorMessage/index.js
+++ GameHub.Web/ClientApp/src/components/HigherOrder/withTimedErrorMessage/index.js
@@ -1,81 +1,35 @@
 import React, { Component } from 'react';
-import { timeout } from '../../../utils/sleep';
+import withPopupOverlay from '../withTimedMessagePopup/withTimedMessagePopup';
 import Popup from '../../Popup';
-import { transition_period } from './styles.scss';
+import './styles.scss';
 
-let show = "timed-error-super-container-show";
-let hide = "timed-error-super-container-hide";
+let showClassNames = "timed-error-super-container-show";
+let hideClassNames = "timed-error-super-container-hide";
+let classNames = "timed-error-super-container";
 
 export default function(WrappedComponent, duration)
 {
-    return class TimedErrorDropdown extends Component
+    class TimedErrorDropdown extends Component
     {
-        constructor(props)
-        {
-            super(props);
-
-            this.state = {
-                message: "",
-                messageTimeoutDurationMS: duration === undefined ? 3000 : duration,
-                render: false
-            };
-        }
-
-        display = async message =>
-        {
-            // don't bother changing if same message as can cause rendering issues.
-            if (this.state.message === message) return;
-
-            if (message === undefined) return;
-        
-            if(this.state.message !== "")
-            {
-                await this.close();
-            }
-
-            this.setState({message:message, render: true}, async () =>
-            {
-                await timeout(this.state.messageTimeoutDurationMS);
-
-                // if the message has changed, dont wipe it as it's the responsibilty of another process now. wiping it can cause shortened messages.
-                if (this.state.message === message)
-                {
-                    this.close();
-                }
-            });
-        }
-
-        close = async () =>
-        {
-            this.setState({render: false});
-
-            await timeout(transition_period);
-
-            this.setState({message: ""});
-        }
-
         render = () =>
         {
-            let renderSpecificClass = this.state.render ? show : hide;
+            let renderSpecificClass = this.props.shouldShowError ? showClassNames : hideClassNames;
+
             return (
                 <div>
-                    <div onClick={this.close}>
-                        {this.state.message !== "" && (
-                            <Popup
-                                superContainerClassNames={"timed-error-super-container " + renderSpecificClass}
-                            >
-                                <span onClick={this.close}>
-                                    {this.state.message}
-                                </span>
-                            </Popup>
-                        )}
+                    <div onClick={this.props.closeError}>
+                        <Popup
+                            superContainerClassNames={[classNames, renderSpecificClass].join(" ")}
+                        >
+                            <span onClick={this.props.closeError}>
+                                {this.props.errorMessage}
+                            </span>
+                        </Popup>
                      </div>
-                    <WrappedComponent 
-                        {...this.props}
-                        displayTimedError={this.display}
-                    />
                 </div>
             );
-        }
+        };
     }
+
+    return withPopupOverlay(WrappedComponent, TimedErrorDropdown, "displayTimedError", "errorMessage", "closeError", "shouldShowError", duration);
 }
---
+++ GameHub.Web/ClientApp/src/components/HigherOrder/withTimedMessagePopup/index.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { timeout } from '../../../utils/sleep';
+import { transition_period } from './styles.scss';
+
+export default function(WrappedComponent, PopupComponent,
+    displayPropName = "display", 
+    messagePropName = "message", 
+    closePropName = "close", 
+    showPropName = "show", 
+    duration = 3000)
+{
+    return class WithTimedPopup extends Component
+    {
+        constructor(props)
+        {
+            super(props);
+
+            this.state = {
+                message: "",
+                messageTimeoutDurationMS: duration,
+                render: false
+            };
+        }
+
+        display = async message =>
+        {
+            // don't bother changing if same message as can cause rendering issues.
+            if (this.state.message === message) return;
+
+            if (message === undefined) return;
+        
+            if(this.state.message !== "")
+            {
+                await this.close();
+            }
+
+            this.setState({message:message, render: true}, async () =>
+            {
+                await timeout(this.state.messageTimeoutDurationMS);
+
+                // if the message has changed, dont wipe it as it's the responsibilty of another process now. wiping it can cause shortened messages.
+                if (this.state.message === message)
+                {
+                    this.close();
+                }
+            });
+        }
+
+        close = async () =>
+        {
+            this.setState({render: false});
+
+            await timeout(transition_period);
+
+            this.setState({message: ""});
+        }
+
+        render = () =>
+        {
+            let PopupComponentProps = {};
+            PopupComponentProps[closePropName] = this.close;
+            PopupComponentProps[showPropName] = this.state.render;
+            PopupComponentProps[messagePropName] = this.state.message;
+
+            let WrappedComponentProps = {};
+            WrappedComponentProps[displayPropName] = this.display;
+
+            return (
+                <div>
+                    <div>
+                        {this.state.message !== "" && (
+                            <PopupComponent {...PopupComponentProps} />
+                        )}
+                     </div>
+                    <WrappedComponent 
+                        {...this.props}
+                        {...WrappedComponentProps}
+                    />
+                </div>
+            );
+        }
+    }
+}
